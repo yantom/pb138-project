b0d534b6ded63dd6c833f4730b725e2b6c6c0fd6
Ondrej Bulla <bulla8ondra@seznam.cz>
2015-06-08 23:43:46 +0200

    Doplnil jsem třídu SchemaValidator pro validaci souboru podle xsd schématu. Náčrt šablony pro vstupní soubory arabského wordnetu. Transformace na převod dokumentu z formátu VisDic na DEBVisDic.

---
+++ src/main/java/pb138/wordnetconvert/backend/SchemaValidator.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pb138.wordnetconvert.backend;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
+import org.w3c.dom.Document;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+/**
+ *
+ * @author bar
+ */
+public class SchemaValidator {
+    class ValidationErrorsHandler implements ErrorHandler{
+
+        @Override
+        public void warning(SAXParseException exception) throws SAXException {
+            Logger.getAnonymousLogger(SchemaValidator.class.getName()).log(Level.INFO,exception.getMessage());
+        }
+
+        @Override
+        public void error(SAXParseException exception) throws SAXException {
+            error = exception.getMessage();
+            throw new SAXException(error);
+        }
+
+        @Override
+        public void fatalError(SAXParseException exception) throws SAXException {
+            error = exception.getMessage();
+            throw new SAXException(error);
+        }
+
+        
+    }
+    private DocumentBuilder docBuilder;
+    private String error;
+    public SchemaValidator(){
+        try {
+            DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+            docBuilder = dbf.newDocumentBuilder();
+            docBuilder.setErrorHandler(new ValidationErrorsHandler());
+        } catch (ParserConfigurationException ex) {
+            System.err.println("Error initializing parser: "+ ex);
+        }
+        
+    }
+    public SchemaValidator(String schemaName){
+        try {
+            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = sf.newSchema(new File("company.xsd"));
+            
+            DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+            
+            dbf.setSchema(schema);
+            docBuilder = dbf.newDocumentBuilder();
+            docBuilder.setErrorHandler(new ValidationErrorsHandler());
+        } catch (SAXException ex) {
+            System.err.println("Invalid schema: " + ex.getMessage());
+            System.exit(-1);
+        } catch (ParserConfigurationException ex) {
+            System.err.println("Parser configuration error: " + ex.getMessage());
+        }
+    }
+    
+    public Boolean validate(String xmlFilename) throws IOException{
+        try {
+            Document doc = docBuilder.parse(new File(xmlFilename));
+        } catch (SAXException ex) {
+            //Logger.getAnonymousLogger(SchemaValidator.class.getName()).log(Level.INFO,ex.getMessage());
+            return false;
+        } 
+        return true;
+    }
+}
