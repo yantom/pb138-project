b1f2498c54d36f0406a54f3ce7d732258abea4c8
Honzator <Honzator@Honzator-PC>
2015-06-10 01:30:41 +0200

    JAXB annotations

--- src/main/java/pb138/wordnetconvert/backend/Synonym.java
+++ src/main/java/pb138/wordnetconvert/backend/Synonym.java
@@ -5,12 +5,22 @@
  */
 package pb138.wordnetconvert.backend;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  *
  * @author Honzator
  */
+@XmlRootElement(name = "SYNONYM")
 public class Synonym {
-    String literal;
-    int sense;
-    String lnote;
+    @XmlElement(name = "LITERAL")
+    private List<Literal> literals = new ArrayList<>();
+
+    public List<Literal> getLiterals() {
+        return literals;
+    }
+    
 }
