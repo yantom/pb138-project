c7bd897c0eb61e5a15ca2b5881708436872ba6c6
Ondrej Bulla <bulla8ondra@seznam.cz>
2015-06-13 11:04:51 +0200

    do arabic wn konverze přidáno provedení XQuery

--- src/main/java/pb138/wordnetconvert/backend/ArabicWordnetConverterImpl.java
+++ src/main/java/pb138/wordnetconvert/backend/ArabicWordnetConverterImpl.java
@@ -22,14 +22,25 @@ public class ArabicWordnetConverterImpl implements WordnetConverter {
 	    validator.validate(sourcePath);
 	}
 	catch(SAXException ex) {
-	    throw new WordnetException(ex.getMessage());
+	    throw new WordnetException("Invalid input file.");
 	}
         catch(IOException ex){
             throw new WordnetException(ex.getMessage());
         }
 	File source = new File(sourcePath);
 	source.renameTo(new File("in"));
-	//TODO execute XQuery
+	
+	Process p;
+	try {
+		p = Runtime.getRuntime().exec("java -cp saxon9he.jar net.sf.saxon.Query -s:in -o:" + destinationPath + " awnToDebvisdic.xq");
+		p.waitFor();
+	}
+	catch(Exception e) {
+	    source = new File("in");
+	    source.renameTo(new File(sourcePath));
+	    throw new WordnetException("Converting failed.");
+	}
+	
 	source = new File("in");
 	source.renameTo(new File(sourcePath));
     }
